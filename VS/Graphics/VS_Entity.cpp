da04d1ed84414b91c78512924e90064a9ef9cf04
Trevor Powell <trevor@gridbug.org>
2012-05-23 22:05:21 +1000

    Fixed vsEntity::Extract to properly unregister from parent

--- VS/Graphics/VS_Entity.cpp
+++ VS/Graphics/VS_Entity.cpp
@@ -29,19 +29,19 @@ vsEntity::~vsEntity()
 		m_parent->RemoveChild(this);
 	else
 		Extract();
-	
+
 	// now, delete our children.  Remember that they'll be extracting themselves from our child list as they're deleted,
 	// so we need to check what the next child is before we delete the previous child!
-	
+
 	vsEntity *child = m_child;
-	
+
 	while(child)
 	{
 		vsEntity *next = child->m_next;
-		
+
 		if ( next == child )
 			next = NULL;
-		
+
 		delete child;
 		child = next;
 	}
@@ -52,7 +52,7 @@ vsEntity::Append( vsEntity *sprite )
 {
 	sprite->m_next = m_next;
 	sprite->m_prev = this;
-	
+
 	m_next->m_prev = sprite;
 	m_next = sprite;
 }
@@ -62,7 +62,7 @@ vsEntity::Prepend( vsEntity *sprite )
 {
 	sprite->m_next = this;
 	sprite->m_prev = m_prev;
-	
+
 	m_prev->m_next = sprite;
 	m_prev = sprite;
 }
@@ -73,10 +73,10 @@ vsEntity::AddChild( vsEntity *sprite )
 	sprite->m_parent = this;
 	sprite->m_next = m_child;
 	sprite->m_prev = NULL;
-	
+
 	if ( m_child )
 		m_child->m_prev = sprite;
-	
+
 	m_child = sprite;
 }
 
@@ -87,35 +87,40 @@ vsEntity::RemoveChild( vsEntity *sprite )
 	{
 		m_child = m_child->m_next;
 	}
-	else
-	{
-		sprite->Extract();
-	}
+	sprite->m_parent = NULL;
+	sprite->Extract();
 }
 
 void
 vsEntity::Extract()
 {
-	if ( m_prev )
-		m_prev->m_next = m_next;
-	if ( m_next )
-		m_next->m_prev = m_prev;
-	
-	m_prev = m_next = this;
+	if ( m_parent )
+	{
+		m_parent->RemoveChild( this );
+	}
+	else
+	{
+		if ( m_prev )
+			m_prev->m_next = m_next;
+		if ( m_next )
+			m_next->m_prev = m_prev;
+
+		m_prev = m_next = this;
+	}
 }
 
 void
 vsEntity::DrawChildren( vsRenderQueue *queue )
 {
 	vsEntity *child = m_child;
-	
+
 	while ( child )
 	{
 		if ( child->OnScreen(g_drawingCameraTransform) )
 		{
 			child->Draw( queue );
 		}
-		
+
 		child = child->m_next;
 	}
 }
@@ -134,11 +139,11 @@ void
 vsEntity::Update( float timeStep )
 {
 	vsEntity *child = m_child;
-	
+
 	while ( child )
 	{
 		vsEntity *next = child->m_next;
-		
+
 		child->Update( timeStep );
 
 		child = next;
@@ -156,19 +161,19 @@ vsEntity::Find( const vsString &name )
 {
 	if ( m_name == name )
 		return this;
-	
+
 	vsEntity *result = NULL;
-	
+
 	for( vsEntity *child = m_child; child; child = child->m_next )
 	{
 		result = child->Find(name);
-		
+
 		if ( result )
 		{
 			break;
 		}
 	}
-	
+
 	return result;
 }
 
@@ -177,20 +182,20 @@ vsEntity::FindEntityAtPosition(const vsVector2D &pos)
 {
 	if ( !m_clickable || !m_visible )
 		return NULL;
-	
+
 	vsEntity *result = NULL;
-	
+
 	vsEntity *child = m_child;
-	
+
 	while ( !result && child )
 	{
 		vsEntity *next = child->m_next;
-		
+
 		result = child->FindEntityAtPosition(pos);
-		
+
 		child = next;
 	}
-		
+
 	return result;
 }
 
