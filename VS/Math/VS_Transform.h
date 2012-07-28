226fb364b98130c95ad1bf2ac5054718aad49fd7
Trevor <trevor@gridbug.org>
2012-07-28 22:21:04 +1000

    Helper functions on vsTransform2D.

--- VS/Math/VS_Transform.h
+++ VS/Math/VS_Transform.h
@@ -19,30 +19,32 @@ class vsTransform2D
 {
 	mutable bool			m_dirty;
 	mutable vsMatrix4x4		m_matrix;
-	
+
 	vsVector2D		m_position;
 	vsAngle			m_angle;
 	vsVector2D		m_scale;
-	
+
 public:
-	
+
 	static vsTransform2D	Zero;
-	
+
 	vsTransform2D();
 	vsTransform2D(const vsVector2D &pos, const vsAngle &angle);
 	vsTransform2D(const vsVector2D &pos, const vsAngle &angle, const vsVector2D &scale);
-    
+
     void            SetTranslation( const vsVector2D &translation );
+    void            SetTranslation( float x, float y ) { SetTranslation( vsVector2D(x,y) ); }
     void            SetAngle( const vsAngle &angle );
     void            SetScale( const vsVector2D &scale );
-	
+    void            SetScale( float x, float y ) { SetScale( vsVector2D(x,y) ); }
+
     const vsVector2D& GetTranslation() const    { return m_position; }
     const vsAngle&  GetAngle() const            { return m_angle; }
     const vsVector2D& GetScale() const          { return m_scale; }
-    
+
 	vsVector2D		ApplyTo( const vsVector2D &v );
 	vsVector2D		ApplyInverseTo( const vsVector2D &v );
-	
+
 	vsTransform2D operator*( const vsTransform2D &o );
 	vsTransform2D	ApplyInverseTo( const vsTransform2D &o );
 
@@ -54,36 +56,36 @@ class vsTransform3D
 	vsQuaternion	m_quaternion;
 	vsVector3D		m_translation;
 	vsVector3D		m_scale;
-	
+
 	mutable bool			m_dirty;
 	mutable vsMatrix4x4		m_matrix;
-	
+
 public:
 	static vsTransform3D	Identity;
-	
+
 	vsTransform3D( const vsQuaternion &quat = vsQuaternion::Identity, const vsVector3D &translation = vsVector3D::Zero, const vsVector3D &scale = vsVector3D::One );	// from a forward and an up vector, we can create a rotation matrix!
-	
+
 	const vsVector3D &		GetTranslation() const		{ return m_translation; }
 	const vsQuaternion &	GetRotation() const			{ return m_quaternion; }
 	const vsVector3D &		GetScale() const			{ return m_scale; }
-	
+
 	void					SetTranslation( const vsVector3D &t ) { m_translation = t; m_dirty = true; }
 	void					SetRotation( const vsQuaternion &q )	{ m_quaternion = q; m_dirty = true; }
 	void					SetScale( const vsVector3D &s )	{ m_scale = s; m_dirty = true; }
 	void					SetScale( float s ) { m_scale.Set(s,s,s); m_dirty = true; }
-	
+
 	const vsMatrix4x4 &		GetMatrix() const;
-	
+
 //	bool			IsIdentity() { return (this == vsTransform3D::Identity); }
-	
+
 	vsTransform3D	Inverse() const;
 	vsTransform3D	Transpose() const;
-	
+
 	void			Set( const vsVector4D &x, const vsVector4D &y, const vsVector4D &z, const vsVector4D &t );
 
 	vsVector3D		ApplyTo( const vsVector3D &v ) const;
 	vsVector3D		ApplyInverseTo( const vsVector3D &v ) const;
-	
+
 	vsTransform3D	operator*( const vsTransform3D &o ) const { return ApplyTo(o); }
 	vsTransform3D	ApplyTo( const vsTransform3D &o ) const;
 	vsTransform3D	ApplyInverseTo( const vsTransform3D &o ) const;
