797ce0b056cc7f20ec40631afdb5d2ebe7543f40
Trevor Powell <trevor@gridbug.org>
2012-12-30 23:30:35 -0800

    Adds math function for converting quaternion to euler angles

--- VS/Math/VS_Quaternion.cpp
+++ VS/Math/VS_Quaternion.cpp
@@ -32,7 +32,7 @@ vsQuaternion::vsQuaternion(float x_in, float y_in, float z_in, float w_in):
 vsQuaternion::vsQuaternion( const vsVector3D &axis, float angle )
 {
 	vsAngle halfAngle( angle * 0.5f );
-	
+
 	w = halfAngle.Cos();
 	x = axis.x * halfAngle.Sin();
 	y = axis.y * halfAngle.Sin();
@@ -42,7 +42,7 @@ vsQuaternion::vsQuaternion( const vsVector3D &axis, float angle )
 vsQuaternion::vsQuaternion(const vsVector3D &forward, const vsVector3D &up)
 {
 	vsMatrix3x3 mat(forward,up);
-	
+
 	Set(mat);
 }
 
@@ -55,7 +55,7 @@ void
 vsQuaternion::Set( const vsMatrix3x3 &mat )
 {
 	float trace = mat.x.x + mat.y.y + mat.z.z;
-	if( trace > 0.f ) 
+	if( trace > 0.f )
 	{
 		float s = vsSqrt(trace+1.f);
 		w = 0.5f * s;
@@ -63,37 +63,37 @@ vsQuaternion::Set( const vsMatrix3x3 &mat )
 		x = (mat[1][2] - mat[2][1]) * s;
 		y = (mat[2][0] - mat[0][2]) * s;
 		z = (mat[0][1] - mat[1][0]) * s;
-	} 
+	}
 	else // diagonal is negative
 	{
-		
+
 		int nxt[3] = {1,2,0};
 		int i = 0;
 		int j,k;
 		float q[4];
-		
+
 		if ( mat[1][1] > mat[0][0] ) i = 1;
 		if ( mat[2][2] > mat[i][i] ) i = 2;
-		
+
 		j = nxt[i];
 		k = nxt[j];
-		
+
 		float s = vsSqrt( (mat[i][i] - (mat[j][j] + mat[k][k])) + 1.0f );
 		q[i] = s * 0.5f;
-		
+
 		if ( s != 0.f )
 			s = 0.5f / s;
-		
+
 		q[3] = (mat[j][k] - mat[k][j]) * s;
 		q[j] = (mat[i][j] + mat[j][i]) * s;
 		q[k] = (mat[i][k] + mat[k][i]) * s;
-		
+
 		x = q[0];
 		y = q[1];
 		z = q[2];
 		w = q[3];
 	}
-	
+
 	NormaliseIfNeeded();
 }
 
@@ -101,20 +101,20 @@ void
 vsQuaternion::Set( const vsEulerAngles &ang )
 {
 	float cr, cp, cy, sr, sp, sy, cpcy, spsy;
-	
+
 	vsAngle halfPitch = ang.pitch * .5f;
 	vsAngle halfYaw = ang.yaw * .5f;
 	vsAngle halfBank = ang.bank * .5f;
-	
+
 	// pitch becomes bank
 	// yaw becomes pitch
 	// bank becomes yaw??
-	
+
 	// calculate trig identities
 	cr = halfPitch.Cos();
 	cp = halfYaw.Cos();
 	cy = halfBank.Cos();
-	
+
 	sr = halfPitch.Sin();
 	sp = halfYaw.Sin();
 	sy = halfBank.Sin();
@@ -123,30 +123,41 @@ vsQuaternion::Set( const vsEulerAngles &ang )
  cr = halfBank.Cos();
  cp = halfPitch.Cos();
  cy = halfYaw.Cos();
- 
+
  sr = halfBank.Sin();
  sp = halfPitch.Sin();
  sy = halfYaw.Sin();
-*/ 
+*/
 	cpcy = cp * cy;
 	spsy = sp * sy;
-	
+
 	w = cr * cpcy + sr * spsy;
 	x = sr * cpcy - cr * spsy;
 	y = cr * sp * cy + sr * cp * sy;
 	z = cr * cp * sy - sr * sp * cy;
 }
 
+vsEulerAngles vsEulerAnglesFromQuaternion( const vsQuaternion &q )
+{
+	vsEulerAngles result;
+
+	result.pitch = vsATan2(2.f * (q.w*q.x - q.y*q.z), 1.f - 2.f*(q.x*q.x + q.z*q.z));
+	result.bank = vsASin(2.f * (q.x*q.y + q.z*q.w));
+	result.yaw = vsATan2(2.f * (q.w*q.y - q.x*q.z), 1.f - 2.f*(q.y*q.y - q.z*q.z));
+
+	return result;
+}
+
 void
 vsQuaternion::Normalise()
 {
 	float sqLength = ( w*w + x*x + y*y + z*z );
-	
+
 	if ( sqLength != 1.f )
 	{
 		float length = vsSqrt(sqLength);
 		float invLength = 1.f / length;
-		
+
 		w *= invLength;
 		x *= invLength;
 		y *= invLength;
@@ -158,12 +169,12 @@ void
 vsQuaternion::NormaliseIfNeeded()
 {
 	float sqLength = ( w*w + x*x + y*y + z*z );
-	
+
 	if ( sqLength > 1.01f || sqLength < 0.99f )
 	{
 		float length = vsSqrt(sqLength);
 		float invLength = 1.f / length;
-		
+
 		w *= invLength;
 		x *= invLength;
 		y *= invLength;
@@ -171,30 +182,30 @@ vsQuaternion::NormaliseIfNeeded()
 	}
 }
 
-vsQuaternion 
+vsQuaternion
 vsQuaternion::operator*(const vsQuaternion &b) const
 {
 	float A, B, C, D, E, F, G, H;
-	
-	
+
+
 	A = (w + x)*(b.w + b.x);
 	B = (z - y)*(b.y - b.z);
-	C = (w - x)*(b.y + b.z); 
+	C = (w - x)*(b.y + b.z);
 	D = (y + z)*(b.w - b.x);
 	E = (x + z)*(b.x + b.y);
 	F = (x - z)*(b.x - b.y);
 	G = (w + y)*(b.w - b.z);
 	H = (w - y)*(b.w + b.z);
-	
+
 	vsQuaternion res;
-	
+
 	res.w = B + (-E - F + G + H) /2;
-	res.x = A - (E + F + G + H)/2; 
-	res.y = C + (E - F + G - H)/2; 
+	res.x = A - (E + F + G + H)/2;
+	res.y = C + (E - F + G - H)/2;
 	res.z = D + (E - F - G + H)/2;
-	
+
 	res.NormaliseIfNeeded();
-	
+
 	return res;
 }
 
@@ -202,16 +213,16 @@ vsQuaternion vsQuaternionSlerp( float alpha, const vsQuaternion &from, const vsQ
 {
 	float	to1[4];
 	float	omega, cosom, sinom, scale0, scale1;
-	
-	
+
+
 	// calc cosine
 	cosom = from.x * to.x + from.y * to.y + from.z * to.z + from.w * to.w;
-	
-	
+
+
 	// adjust signs (if necessary)
 	if ( cosom <0.0 )
-	{ 
-		cosom = -cosom; 
+	{
+		cosom = -cosom;
 		to1[0] = - to.x;
 		to1[1] = - to.y;
 		to1[2] = - to.z;
@@ -222,35 +233,35 @@ vsQuaternion vsQuaternionSlerp( float alpha, const vsQuaternion &from, const vsQ
 		to1[2] = to.z;
 		to1[3] = to.w;
 	}
-	
-	
+
+
 	// calculate coefficients
-	
-	
+
+
 	if ( (1.0f - cosom) > 0.05f ) {
 		// standard case (slerp)
 		omega = vsACos(cosom);
 		sinom = vsSin(omega);
 		scale0 = vsSin((1.0f - alpha) * omega) / sinom;
 		scale1 = vsSin(alpha * omega) / sinom;
-		
-		
-	} else {        
-		// "from" and "to" quaternions are very close 
+
+
+	} else {
+		// "from" and "to" quaternions are very close
 	    //  ... so we can do a linear interpolation
 		scale0 = 1.0f - alpha;
 		scale1 = alpha;
 	}
-	
+
 	vsQuaternion result;
 	// calculate final values
 	result.x = scale0 * from.x + scale1 * to1[0];
 	result.y = scale0 * from.y + scale1 * to1[1];
 	result.z = scale0 * from.z + scale1 * to1[2];
 	result.w = scale0 * from.w + scale1 * to1[3];
-	
+
 	result.NormaliseIfNeeded();
-	
+
 	return result;
 }
 
@@ -260,7 +271,7 @@ vsQuaternion::ApplyTo( const vsVector3D &in ) const
 {
 	vsMatrix3x3 mat;
 	mat.Set(*this);
-	
+
 	return mat.ApplyTo(in);
 }
 
