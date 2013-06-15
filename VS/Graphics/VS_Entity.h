2c732a5d39d5c6b2a22c7412266e29694731f1af
Trevor Powell <trevor@gridbug.org>
2013-06-16 00:24:58 +1000

    Adds a setter for vsEntity name.

--- VS/Graphics/VS_Entity.h
+++ VS/Graphics/VS_Entity.h
@@ -21,42 +21,42 @@ class vsSceneDraw;
 class vsEntity
 {
 protected:
-	
+
 	vsString		m_name;
-	
+
 	vsEntity *		m_next;
 	vsEntity *		m_prev;
-	
+
 	vsEntity *		m_parent;
 	vsEntity *		m_child;
-	
+
 	bool			m_visible;
 	bool			m_clickable;
-	
+
 	void			DrawChildren( vsRenderQueue *queue );
-	
+
 public:
 		vsEntity();
 	virtual ~vsEntity();
-	
+
 	virtual void	Update( float timeStep );
 	virtual void	Draw( vsRenderQueue *queue );
 	virtual void	DynamicDraw( vsRenderQueue *queue ) {UNUSED(queue);}
-	
+
 	virtual bool	OnScreen(const vsTransform2D & /*cameraTrans*/) { return true; }
-	
+
 	void			RegisterOnScene(int scene);
 #if defined(_DEBUG)
 	void			RegisterOnDebugScene();
 #endif // _DEBUG
 	void			Unregister() { Extract(); }
-	
+
 	void			SetVisible(bool visible) { m_visible = visible; }
 	bool			GetVisible() { return m_visible; }
 	bool			IsVisible() { return GetVisible(); }
-	
+
 	void			SetClickable(bool clickable) { m_clickable = clickable; }
-	
+
 	vsEntity *		GetNext() { return m_next; }
 	vsEntity *		GetPrev() { return m_prev; }
 	vsEntity *		GetParent() { return m_parent; }
@@ -64,13 +64,15 @@ public:
 	void			Prepend( vsEntity *sprite );
 	void			AddChild( vsEntity *sprite );
 	void			RemoveChild( vsEntity *sprite );
-	
+
+	void			SetName( const vsString &name ) { m_name = name; }
+	const vsString&	GetName() { return m_name; }
 	vsEntity *		Find( const vsString &name );
 	virtual vsEntity *	FindEntityAtPosition(const vsVector2D &pos);	// 'pos' is in parent coordinates!  Returns true if we swallowed the 'click' action.
-	
+
 	void			MoveToTop();
 	void			MoveToBottom();
-	
+
 	void			Extract();
 };
 
