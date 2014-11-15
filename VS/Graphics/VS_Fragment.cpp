fe004d503fe3fb8ff9dad9e6bd921fc5b226cb9e
Trevor Powell <trevor@gridbug.org>
2014-11-15 11:22:32 +1100

    Add support for loading vertex buffers.
    Really not happy with this code -- it really demonstrates the need for a
    generic freeform buffer system which doesn't necessarily have pre-defined
    attributes.  Need to figure out what that would look like, because this
    sort of loading code is horrid.

--- VS/Graphics/VS_Fragment.cpp
+++ VS/Graphics/VS_Fragment.cpp
@@ -41,6 +41,185 @@ vsFragment::Load( vsRecord *record )
 			vsDelete( result->m_material );
 			result->m_material = new vsMaterial( sr->GetToken(0).AsString() );
 		}
+		else if ( srLabel == "PBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::P *va = new vsRenderBuffer::P[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 3, "Wrong number of tokens in P buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
+		else if ( srLabel == "PCBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::PC *va = new vsRenderBuffer::PC[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 7, "Wrong number of tokens in PC buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+					va[id].color.Set(s->GetToken(3).AsFloat(), s->GetToken(4).AsFloat(), s->GetToken(5).AsFloat(), s->GetToken(6).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
+		else if ( srLabel == "PNBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::PN *va = new vsRenderBuffer::PN[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 6, "Wrong number of tokens in PN buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+					va[id].normal.Set(s->GetToken(3).AsFloat(), s->GetToken(4).AsFloat(), s->GetToken(5).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
+		else if ( srLabel == "PCTBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::PCT *va = new vsRenderBuffer::PCT[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 9, "Wrong number of tokens in PCT buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+					va[id].color.Set(s->GetToken(3).AsFloat(), s->GetToken(4).AsFloat(), s->GetToken(5).AsFloat(), s->GetToken(6).AsFloat());
+					va[id].texel.Set(s->GetToken(7).AsFloat(), s->GetToken(8).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
+		else if ( srLabel == "PNTBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::PNT *va = new vsRenderBuffer::PNT[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 8, "Wrong number of tokens in PNT buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+					va[id].normal.Set(s->GetToken(3).AsFloat(), s->GetToken(4).AsFloat(), s->GetToken(5).AsFloat());
+					va[id].texel.Set(s->GetToken(6).AsFloat(), s->GetToken(7).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
+		else if ( srLabel == "PCNBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::PCN *va = new vsRenderBuffer::PCN[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 10, "Wrong number of tokens in PCNT buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+					va[id].color.Set(s->GetToken(3).AsFloat(), s->GetToken(4).AsFloat(), s->GetToken(5).AsFloat(), s->GetToken(6).AsFloat());
+					va[id].normal.Set(s->GetToken(7).AsFloat(), s->GetToken(8).AsFloat(), s->GetToken(9).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
+		else if ( srLabel == "PCNTBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int arrayCount = sr->GetChildCount();
+			if ( arrayCount )
+			{
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				vsRenderBuffer::PCNT *va = new vsRenderBuffer::PCNT[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					vsRecord *s = sr->GetChild(id);
+					vsAssert( s->GetTokenCount() == 12, "Wrong number of tokens in PCNT buffer" );
+					va[id].position.Set(s->GetToken(0).AsFloat(), s->GetToken(1).AsFloat(), s->GetToken(2).AsFloat());
+					va[id].color.Set(s->GetToken(3).AsFloat(), s->GetToken(4).AsFloat(), s->GetToken(5).AsFloat(), s->GetToken(6).AsFloat());
+					va[id].normal.Set(s->GetToken(7).AsFloat(), s->GetToken(8).AsFloat(), s->GetToken(9).AsFloat());
+					va[id].texel.Set(s->GetToken(10).AsFloat(), s->GetToken(11).AsFloat());
+				}
+
+				buffer->SetArray(va, arrayCount);
+				vsDeleteArray(va);
+
+				result->AddBuffer(buffer);
+			}
+		}
 		else if ( srLabel == "VertexBuffer" )
 		{
 			// make ourselves a vsRenderBuffer to contain this vertex data.
@@ -67,6 +246,30 @@ vsFragment::Load( vsRecord *record )
 				result->AddBuffer(buffer);
 			}
 		}
+		else if ( srLabel == "IndexBuffer" )
+		{
+			// make ourselves a vsRenderBuffer to contain this vertex data.
+			int childCount = sr->GetChildCount();
+			vsAssert( childCount == 1, "Wrong number of children of IndexBuffer" );
+			{
+				vsRecord *s = sr->GetChild(0);
+				int arrayCount = s->GetTokenCount();
+				vsRenderBuffer *buffer = new vsRenderBuffer;
+
+				uint16_t *ind = new uint16_t[arrayCount];
+				vsRecord subRecord;
+
+				for ( int id = 0; id < arrayCount; id++ )
+				{
+					ind[id] = s->GetToken(id).AsInteger();
+				}
+
+				buffer->SetArray(ind, arrayCount);
+				vsDeleteArray(ind);
+
+				result->AddBuffer(buffer);
+			}
+		}
 		else if ( srLabel == "DisplayList" )
 		{
 			vsDisplayList *loader = new vsDisplayList(1024*50);
@@ -85,13 +288,31 @@ vsFragment::Load( vsRecord *record )
 						loader->VertexBuffer( result->m_bufferList[bufferId] );
 					}
 				}
-				if ( ssrLabel == "BindNormalBuffer" )
+				else if ( ssrLabel == "BindNormalBuffer" )
 				{
 					int bufferId = ssr->GetToken(0).AsInteger();
 
 					if ( bufferId < result->m_bufferList.ItemCount() )
 					{
 						loader->NormalBuffer( result->m_bufferList[bufferId] );
+					}
+				}
+				else if ( ssrLabel == "BindBuffer" )
+				{
+					int bufferId = ssr->GetToken(0).AsInteger();
+
+					if ( bufferId < result->m_bufferList.ItemCount() )
+					{
+						loader->BindBuffer( result->m_bufferList[bufferId] );
+					}
+				}
+				else if ( ssrLabel == "TriangleListBuffer" )
+				{
+					int bufferId = ssr->GetToken(0).AsInteger();
+
+					if ( bufferId < result->m_bufferList.ItemCount() )
+					{
+						loader->TriangleListBuffer( result->m_bufferList[bufferId] );
 					}
 				}
 				else
