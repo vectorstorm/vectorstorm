ba6ef3078b274bba0eb7fae3f11b8eeffc08556c
Trevor Powell <trevor@vectorstorm.org>
2015-01-13 22:09:41 +1100

    Fix for Win32 build.

--- VS/VS_Prefix.h
+++ VS/VS_Prefix.h
@@ -93,12 +93,14 @@ void operator delete[](void* pointer, const char* file, size_t line);
 
 #if defined(_WIN32)
 
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
+typedef signed __int8 int8_t;
+typedef unsigned __int8 uint8_t;
+typedef signed __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+typedef signed __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef signed __int64 int64_t;
+typedef unsigned __int64 uint64_t;
 
 #else // __GNUC__, __APPLE_CC__, and probably everything else?
 
