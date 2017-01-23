67787edcbe291979437efefd1c02012ea3b4b395
Trevor Powell <trevor@vectorstorm.org>
2017-01-23 12:24:27 +1100

    Clamp render texture access to edges, same as MSAA renderbuffers.
    TODO is to make this optional via something in the settings struct.  But
    for right now, make non-AA rendering work the same way as AA rendering.

--- VS/Graphics/VS_RenderTarget.cpp
+++ VS/Graphics/VS_RenderTarget.cpp
@@ -323,8 +323,9 @@ vsSurface::vsSurface( const Settings& settings, bool depthOnly, bool multisample
 
 			//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			//glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-			//glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+
+			glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+			glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 
 			if ( depthCompare )
 			{
