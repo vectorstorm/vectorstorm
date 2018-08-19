56a1c76d8cc4e1431fee07bc5d0fac4b6c20b7ee
Trevor Powell <trevor@vectorstorm.org>
2018-08-19 22:23:58 +1000

    Remove empty log message when compressing a PNG.

--- VS/Utils/VS_Image.cpp
+++ VS/Utils/VS_Image.cpp
@@ -303,7 +303,6 @@ vsImage::BakePNG(int compression)
 	const int pngDataSize = 1024*1024*10;
 	char* pngData = new char[pngDataSize];
 	SDL_RWops *dst = SDL_RWFromMem(pngData, pngDataSize);
-	vsLog( "%s", SDL_GetError() );
 	int retval = IMG_SavePNG_RW(image,
 			dst,
 			false);
