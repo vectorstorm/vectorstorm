990d3aff15759adaffc1686851dfe50b4e7438ac
Trevor Powell <trevor@vectorstorm.org>
2019-01-29 23:25:37 +1100

    Initial POC for spitting out a backtrace under MINGW.
    This simple approach uses dbghlp to print a simple backtrace, up to 25
    levels deep.  Note that MinGW does NOT provide symbols;  it's just
    printing the PC at each stack level.
    
    This means that I'd need to keep map files around for each build, and
    probably write a tool for looking up the stack traces automatically,
    so I can decode what's meant by the stack traces.
    
    Which is annoying, but *does* allow me to output backtraces without
    actually crashing the program.  And I was wanting to strip debugging
    information out of the builds *anyway*.

--- VS/Utils/VS_Backtrace.cpp
+++ VS/Utils/VS_Backtrace.cpp
@@ -13,9 +13,79 @@
 #ifdef _WIN32
 
 #include "exchndl.h"
+#include <dbghelp.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 void vsBacktrace()
 {
+	HANDLE process = GetCurrentProcess();
+	HANDLE thread = GetCurrentThread();
+
+	CONTEXT context;
+	memset(&context, 0, sizeof(CONTEXT));
+	context.ContextFlags = CONTEXT_FULL;
+	RtlCaptureContext(&context);
+
+	SymInitialize(process, NULL, TRUE);
+
+	DWORD image;
+	STACKFRAME64 stackframe;
+	ZeroMemory(&stackframe, sizeof(STACKFRAME64));
+
+#ifdef _M_IX86
+	image = IMAGE_FILE_MACHINE_I386;
+	stackframe.AddrPC.Offset = context.Eip;
+	stackframe.AddrPC.Mode = AddrModeFlat;
+	stackframe.AddrFrame.Offset = context.Ebp;
+	stackframe.AddrFrame.Mode = AddrModeFlat;
+	stackframe.AddrStack.Offset = context.Esp;
+	stackframe.AddrStack.Mode = AddrModeFlat;
+#elif _M_X64
+	image = IMAGE_FILE_MACHINE_AMD64;
+	stackframe.AddrPC.Offset = context.Rip;
+	stackframe.AddrPC.Mode = AddrModeFlat;
+	stackframe.AddrFrame.Offset = context.Rsp;
+	stackframe.AddrFrame.Mode = AddrModeFlat;
+	stackframe.AddrStack.Offset = context.Rsp;
+	stackframe.AddrStack.Mode = AddrModeFlat;
+#elif _M_IA64
+	image = IMAGE_FILE_MACHINE_IA64;
+	stackframe.AddrPC.Offset = context.StIIP;
+	stackframe.AddrPC.Mode = AddrModeFlat;
+	stackframe.AddrFrame.Offset = context.IntSp;
+	stackframe.AddrFrame.Mode = AddrModeFlat;
+	stackframe.AddrBStore.Offset = context.RsBSP;
+	stackframe.AddrBStore.Mode = AddrModeFlat;
+	stackframe.AddrStack.Offset = context.IntSp;
+	stackframe.AddrStack.Mode = AddrModeFlat;
+#endif
+
+	for (int i = 0; i < 25; i++) {
+
+		BOOL result = StackWalk64(
+				image, process, thread,
+				&stackframe, &context, NULL,
+				SymFunctionTableAccess64, SymGetModuleBase64, NULL);
+
+		if (!result) { break; }
+
+		char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
+		PSYMBOL_INFO symbol = (PSYMBOL_INFO)buffer;
+		symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+		symbol->MaxNameLen = MAX_SYM_NAME;
+
+		DWORD64 displacement = 0;
+		if (SymFromAddr(process, stackframe.AddrPC.Offset, &displacement, symbol)) {
+			printf("[%i] %s\n", i, symbol->Name);
+		} else {
+			printf("[%i] %#010x\n", i, (unsigned int)stackframe.AddrPC.Offset);
+		}
+
+	}
+
+	SymCleanup(process);
 }
 
 void vsInstallBacktraceHandler()
