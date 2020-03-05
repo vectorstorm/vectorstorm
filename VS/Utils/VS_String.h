8aa4d0582b4f1182b0822bda78dd3b50b0983d7c
Trevor Powell <trevor@vectorstorm.org>
2020-03-05 23:07:08 +1100

    Adds 'vsFormatLoc' for localisation-friendly formatting.
    This uses libfmt's fmt::format() function, which has a syntax like this:
    
    `vsFormatLoc("Foo {1} {0}", "quux", "baz");`, which would return the
    string "Foo baz quux";  the numbered sections refer to the order of the
    arguments.  Very handy during localisation where some languages may
    require variables to be substituted in a different order than other
    languages!
    
    It also supports fancier things like:
    
    `vsFormatLoc("Foo {baz} {quux}", fmt::arg("quux","QUUX"),
    fmt::arg("baz","BAZ"));`, which would give us "Foo BAZ QUUX".  Named
    substitutions are even more awesome for translaters!

--- VS/Utils/VS_String.h
+++ VS/Utils/VS_String.h
@@ -14,7 +14,24 @@
 typedef std::string vsString;
 
 // vsString vsFormatString( const char* format, ... );
+//
+// For historical reasons, vsFormatString() uses a printf-style format,
+// fully supporting POSIX argument position extensions (even on non-POSIX
+// platforms, yay!)
 #define vsFormatString fmt::sprintf
+
+// vsFormatLoc uses a new-style Python-like format.  This lets you do
+// this:
+//
+// vsString string = vsFormatLocString("{1} {0}!", "World", "Hello");
+//
+// or for maximum localiser-friendliness, even lets you do this:
+//
+// vsString string = vsFormatLocString("{hello} {world}!", fmt::arg("world","World"), fmt::arg("hello","Hello"));
+// or even this:
+// vsString string = vsFormatLocString("{hello} {world}!", "world"_a="World", "hello"_a="Hello");
+//
+#define vsFormatLoc fmt::format
 
 vsString vsNumberString(int number);
 
