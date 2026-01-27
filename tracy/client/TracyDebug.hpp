57805fb1ed8dfabfa7a2953533c89035ed6ffe84
Trevor Powell <trevor@vectorstorm.com.au>
2026-01-27 16:11:17 +1100

    Upgrade tracy profiler integration to 0.13.1

--- tracy/client/TracyDebug.hpp
+++ tracy/client/TracyDebug.hpp
@@ -1,11 +1,6 @@
 #ifndef __TRACYPRINT_HPP__
 #define __TRACYPRINT_HPP__
 
-// tracy doesn't care about these warnings, so exclude them.
-#pragma GCC diagnostic ignored "-Wunused-function"
-#pragma GCC diagnostic ignored "-Wunused-result"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-
 #ifdef TRACY_VERBOSE
 #  include <stdio.h>
 #  define TracyDebug(...) fprintf( stderr, __VA_ARGS__ );
